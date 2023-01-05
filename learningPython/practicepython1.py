221c60509c835c4b162517bcdabfc46f8d9555c1
i-am-star <adannaya649@gmail.com>
2023-01-05 18:17:49 +0100

    declaring variables and receiving inputs from users.

---
+++ learningPython/practicepython1.py
@@ -0,0 +1,11 @@
+#variables, expressions and statements.
+# declaring a variable
+# to declare a variable in python
+name = "lucy"
+print("hello", name)
+
+# to receive and print user input
+firstName = input("Enter Firstname:")
+print("Hello", firstName)
+
+
