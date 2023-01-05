bec94e0c5ba71233079163b5332b6560b2a08fed
i-am-star <adannaya649@gmail.com>
2023-01-05 19:08:42 +0100

    variables and user inputs.

--- learningPython/practicepython1.py
+++ learningPython/practicepython1.py
@@ -6,6 +6,10 @@ print("hello", name)
 
 # to receive and print user input
 firstName = input("Enter Firstname:")
-print("Hello", firstName)
+
+#to receive numeric inputs from users
+age = int(input("How old are you?"))
+height = float(input("What is your height?"))
+print("Hello", firstName, "you are", age, "years old", "and your height is", height)
 
 
