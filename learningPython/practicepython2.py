46adff2008f906ce9dc8df37426f0b0d5590150e
i-am-star <adannaya649@gmail.com>
2023-01-06 09:26:34 +0100

    Operators

---
+++ learningPython/practicepython2.py
@@ -0,0 +1,37 @@
+#Operators
+#arithemetic operators
+a = 5
+b = 4
+result = a + b
+print( a, "+", b, "=", result)
+
+#performing simple addition with user input
+a = int(input("Type a number:"))
+b = int(input("Type a second number:"))
+result = a + b
+print( a, "+", b, "=", result)
+
+#boolean and comparison operators
+num = 60
+print(num >= 60)
+
+#compound operator
+x = 6
+y = 9
+x += y
+print(x)
+
+#Logical operators
+
+#AND OPERATOR
+x = 26
+y = 39
+print(x >= 26 and y >= 39)
+
+#OR OPERATOR 
+print(x >= 26 or y < 30)
+
+#NOT OPERATOR
+x = True 
+print(x, not True)
+
