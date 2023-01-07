9b197f7f9b8cec8cf08912bb5a033ab544bd99df
i-am-star <adannaya649@gmail.com>
2023-01-07 15:30:04 +0100

    loops

--- learningPython/practicepython4.py
+++ learningPython/practicepython4.py
@@ -9,3 +9,20 @@ while (counter > 0):
 if (counter < 0):
     print ("Game over")
 
+#using while loop to create a program that accepts input from users and multiplies it.
+
+num = int(input("Enter a number:"))
+counter = 0
+while counter <= 20:
+          result = ( num + counter)
+          print(num, "*", counter, "=", result)
+          counter = counter + 1
+
+
+#for loop
+text = "life"
+for character in text:
+    print(text)
+
+
+
