3c5caa4558e7ea5b91ff175425fdcdd4f6e173d7
i-am-star <adannaya649@gmail.com>
2023-01-07 12:23:56 +0100

    loops

---
+++ learningPython/practicepython4.py
@@ -0,0 +1,11 @@
+#LOOPS
+#WHILE LOOP
+# A program that prints happy new year after a countdown.
+
+counter = 10
+while (counter > 0):
+     counter = counter - 1
+     print (counter)
+if (counter < 0):
+    print ("Game over")
+
