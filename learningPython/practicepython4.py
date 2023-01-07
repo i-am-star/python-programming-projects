853315d48d2b4b2d73b801f5bfd9f39fad9b27b2
i-am-star <adannaya649@gmail.com>
2023-01-07 22:53:34 +0100

    loops

--- learningPython/practicepython4.py
+++ learningPython/practicepython4.py
@@ -1,28 +1,27 @@
-#LOOPS
-#WHILE LOOP
-# A program that prints happy new year after a countdown.
+"""
+LOOPS
+WHILE LOOP
+"""
 
-counter = 10
-while (counter > 0):
-     counter = counter - 1
-     print (counter)
-if (counter < 0):
+counter = int(input("Enter Number:"))
+
+while counter > 0:
+    counter = counter - 1
+    print (counter)
+if counter < 0:
     print ("Game over")
 
 #using while loop to create a program that accepts input from users and multiplies it.
 
 num = int(input("Enter a number:"))
-counter = 0
+COUNTER = 0
 while counter <= 20:
-          result = ( num + counter)
-          print(num, "*", counter, "=", result)
-          counter = counter + 1
+    result = ( num * counter)
+    print(num, "*", counter, "=", result)
+    counter = counter + 1
 
 
 #for loop
-text = "life"
-for character in text:
-    print(text)
-
-
-
+te_st = "life"
+for character in te_st:
+    print(te_st)
